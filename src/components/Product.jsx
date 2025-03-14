00c1c75918527fbab01825587fa2243006d32ce5
NeginZolghadr <negin.zolghadrr@gmail.com>
2025-03-14 05:50:08 +0330

    final3

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,55 +3,18 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
 import { useContext, useMemo } from "react";
 import { CartContext } from "../App";
 
-export default function Product({ id, title, price, image }) {
+export default function Product({ id, title, price, image,buttons }) {
   const navigate = useNavigate();
   const { cart, setCart } = useContext(CartContext);
-
-  const quantity = useMemo(() => {
-    return cart?.find((item) => item.id === id)?.quantity;
-  }, [cart]);
-
   function singleProductHandler() {
     navigate(`/product/${id}`);
   }
-
-  function handleIncrease(evt) {
-    evt.stopPropagation();
-    setCart((cart) => {
-      const isInclude = cart.find((item) => item.id === id);
-      if (isInclude) {
-        return cart.map((item) =>
-          item.id == id ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      } else {
-        return [...cart, { id, quantity: 1 }];
-      }
-    });
-  }
-
-  function handleDecrease(evt) {
-    evt.stopPropagation();
-    setCart((cart) => {
-      const isInclude = cart?.find((item) => item.id === id);
-      if (isInclude) {
-        if (isInclude.quantity > 0) {
-          return cart.map((item) =>
-            item.id == id ? { ...item, quantity: item.quantity - 1 } : item
-          );
-        } else {
-          return;
-        }
-      } else {
-        return;
-      }
-    });
-  }
+ 
   return (
     <Card
       sx={{ border: "2px solid #47126b" ,marginTop:"10px",width:"250px"}}
@@ -72,31 +35,7 @@ export default function Product({ id, title, price, image }) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button
-          size="small"
-          sx={{
-            backgroundColor: "#47126b",
-            color: "white",
-            height: "30px",
-            borderRadius: "10px",
-          }}
-          onClick={handleIncrease}
-        >
-          +
-        </Button>
-        <span>{quantity||0}</span>
-        <Button
-          size="small"
-          sx={{
-            backgroundColor: "#47126b",
-            color: "white",
-            height: "30px",
-            borderRadius: "10px",
-          }}
-          onClick={handleDecrease}
-        >
-          -
-        </Button>
+       {buttons}
       </CardActions>
     </Card>
   );
--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,7 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import Product from "../components/Product";
 import { CartContext } from "../App";
+import Button from "@mui/material/Button";
 
 export default function SingleProductPage() {
   const params = useParams();
@@ -13,6 +14,44 @@ export default function SingleProductPage() {
     .then(res => res.json())
     .then(data => setSingleCart(data));
   }, []);
+
+  const quantity = useMemo(() => {
+    return cart?.find((item) => item.id === params.id)?.quantity;
+  }, [cart]);
+
+  
+
+  function handleIncrease(evt) {
+    evt.stopPropagation();
+    setCart((cart) => {
+      const isInclude = cart.find((item) => item.id === params.id);
+      if (isInclude) {
+        return cart.map((item) =>
+          item.id == params.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        return [...cart, { id:params.id, quantity: 1 }];
+      }
+    });
+  }
+
+  function handleDecrease(evt) {
+    evt.stopPropagation();
+    setCart((cart) => {
+      const isInclude = cart?.find((item) => item.id === params.id);
+      if (isInclude) {
+        if (isInclude.quantity > 0) {
+          return cart.map((item) =>
+            item.id == params.id ? { ...item, quantity: item.quantity - 1 } : item
+          );
+        } else {
+          return;
+        }
+      } else {
+        return;
+      }
+    });
+  }
   return (
     <div className="flex justify-center mt-[10px] mb-[20px]">
     <Product
@@ -20,6 +59,32 @@ export default function SingleProductPage() {
       title={singleCart.title}
       price={singleCart.price}
       image={singleCart.image}
+      buttons={<>
+       <Button
+          size="small"
+          sx={{
+            backgroundColor: "#47126b",
+            color: "white",
+            height: "30px",
+            borderRadius: "10px",
+          }}
+          onClick={handleIncrease}
+        >
+          +
+        </Button>
+        <span>{quantity||0}</span>
+        <Button
+          size="small"
+          sx={{
+            backgroundColor: "#47126b",
+            color: "white",
+            height: "30px",
+            borderRadius: "10px",
+          }}
+          onClick={handleDecrease}
+        >
+          -
+        </Button></>}
     />
     </div>
   );
